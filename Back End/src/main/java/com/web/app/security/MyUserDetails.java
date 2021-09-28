82af051dd533644a076a0d4c75d024e822689d7a
Elmehdi Amlou <90270530+elmehdiamlou@users.noreply.github.com>
2021-09-28 23:17:40 +0100

    First Commit

--- Back End/src/main/java/com/web/app/security/MyUserDetails.java
+++ Back End/src/main/java/com/web/app/security/MyUserDetails.java
@@ -14,10 +14,7 @@ import com.web.app.entity.Role;
 import com.web.app.entity.User;
 
 public class MyUserDetails implements UserDetails {
-
-	/**
-	 * 
-	 */
+  
 	private static final long serialVersionUID = 1L;
 	
 	@Autowired
